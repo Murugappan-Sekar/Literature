c7c6b7cdeb376e8691520474f9446f23e966ad8a
murugappan <muralidaranmuruga@gmail.com>
2018-01-13 07:34:19 +0530

    added IP handler

--- app/src/main/java/com/example/murugappansekar/literature/Startup.java
+++ app/src/main/java/com/example/murugappansekar/literature/Startup.java
@@ -1,13 +1,28 @@
 package com.example.murugappansekar.literature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Startup extends AppCompatActivity {
 
+    Button bHost,bJoin,bDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        bHost=(Button)findViewById(R.id.bHost);
+        bJoin=(Button)findViewById(R.id.bJoin);
+        bDetails=(Button)findViewById(R.id.bDetails);
+
+        bHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Startup.this,NetworkInfo.getIPAddress(true), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
